8140f0a43ed67623423ac6b920543775708b5b57
jimmijameslinux <jj885928@gmail.com>
2025-04-20 20:23:12 +0530

    Updating

--- src/utility/globalPath.js
+++ src/utility/globalPath.js
@@ -1,3 +1,4 @@
-const gpath = 'https://videoplayerbackend-cby7.onrender.com';
+const gpath = 'https://videoplayerbackend-21eq.onrender.com';
+// https://videoplayerbackend-21eq.onrender.com
 // const gpath = 'http://localhost:5000';
 export default gpath;
